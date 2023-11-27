091731a95fea0c018dc3d1b6c8c7781930eaca2e
jashgopani <jbgopani@ncsu.edu>
2023-11-27 02:58:06 -0500

    Refactored Sidebar class component

--- frontend/src/sidebar/Sidebar.js
+++ frontend/src/sidebar/Sidebar.js
@@ -1,42 +1,43 @@
-import React, { Component } from 'react'
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/css/fontawesome.css'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
+import React from 'react';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 
-import '../static/Sidebar.css'
-export default class Sidebar extends Component {
-  render() {
-    return (
-      <div class="left-nav">
-        <div class="left-nav-item">
-          <div onClick={() => this.props.switchPage('SearchPage')}>
-            <i class="fas fa-search left-nav-icon"></i>
-            <span class="left-nav-label">Search</span>
-          </div>
-          <div onClick={() => this.props.switchPage('ManageResumePage')}>
-            <i class="fas fa-folder left-nav-icon"></i>
-            <span class="left-nav-label">Manage</span>
-          </div>
-          <div onClick={() => this.props.switchPage('MatchesPage')}>
-            <i class="fas fa-check-double left-nav-icon"></i>
-            <span class="left-nav-label">Matches</span>
-          </div>
-          <div onClick={() => this.props.switchPage('ApplicationPage')}>
-            <i class="fas fa-file-alt left-nav-icon"></i>
-            <span class="left-nav-label">Applications</span>
-          </div>
-          <div onClick={() => this.props.switchPage('ProfilePage')}>
-            <i class="fas fa-user-alt left-nav-icon"></i>
-            <span class="left-nav-label">Profile</span>
-          </div>
-          <div onClick={() => this.props.handleLogout()}>
-            <i class="fas fa-sign-out-alt left-nav-icon"></i>
-            <span class="left-nav-label">LogOut</span>
-          </div>
-        </div>
-      </div>
-    )
-  }
-}
+import '../static/Sidebar.css';
+
+const Sidebar = ({ switchPage, handleLogout }) => {
+	return (
+		<div className='left-nav'>
+			<div className='left-nav-item'>
+				<div onClick={() => switchPage('SearchPage')}>
+					<i className='fas fa-search left-nav-icon'></i>
+					<span className='left-nav-label'>Search</span>
+				</div>
+				<div onClick={() => switchPage('ManageResumePage')}>
+					<i className='fas fa-folder left-nav-icon'></i>
+					<span className='left-nav-label'>Manage</span>
+				</div>
+				<div onClick={() => switchPage('MatchesPage')}>
+					<i className='fas fa-check-double left-nav-icon'></i>
+					<span className='left-nav-label'>Matches</span>
+				</div>
+				<div onClick={() => switchPage('ApplicationPage')}>
+					<i className='fas fa-file-alt left-nav-icon'></i>
+					<span className='left-nav-label'>Applications</span>
+				</div>
+				<div onClick={() => switchPage('ProfilePage')}>
+					<i className='fas fa-user-alt left-nav-icon'></i>
+					<span className='left-nav-label'>Profile</span>
+				</div>
+				<div onClick={() => handleLogout()}>
+					<i className='fas fa-sign-out-alt left-nav-icon'></i>
+					<span className='left-nav-label'>LogOut</span>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Sidebar;
